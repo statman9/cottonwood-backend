1f53208548c3083021babd2bce23b1a831a93280
Josh Sorensen <joshua.sorensen9@gmail.com>
2018-03-26 17:49:49 -0600

    timestamp add

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,6 +1,7 @@
 'use strict';
 const db        = require('../database')('ember_example');
 const Router    = require('express').Router;
+const mysql     = require('mysql');
 
 const router = new Router();
 module.exports = router;
@@ -22,7 +23,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    db.query("INSERT INTO posts SET id='UUID()', title='"+ req.body.title +"', content='"+req.body.content+"', created_at='now()'",
+    db.query("INSERT INTO posts SET id='UUID()', title='"+ req.body.title +"', content='"+req.body.content+"', created_at=" + mysql.raw('CURRENT_TIMESTAMP()'),
     (err, result) => {
         if (err) {throw err;}
         res.json(result);
@@ -30,15 +31,20 @@ router.post('/', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-    db.delete("DELETE FROM posts WHERE id='" + req.params.id + "'", 
+    db.query("UPDATE posts SET deleted_at=" + mysql.raw('CURRENT_TIMESTAMP()') + "WHERE id='" + req.params.id + "'",
     (err, result) => {
         if (err) {throw err;}
-        res.json(result);
-    });
+        db.query("DELETE FROM posts WHERE id='" + req.params.id + "'", 
+        (error, result2) => {
+            if (error) {throw error;}
+            res.json(result);
+        });
+    })
+    
 });
 
 router.put('/:id', (req, res) => {
-    db.query("UPDATE posts SET title=?, content=?, updated_at='now()' WHERE id= ?", [req.body.title, req.body,content, req.params.id],
+    db.query("UPDATE posts SET title=?, content=?, updated_at=" + mysql.raw('CURRENT_TIMESTAMP()') + " WHERE id= ?", [req.body.title, req.body,content, req.params.id],
     (err, result) => {
         if (err) {throw err;}
         res.json(result);
